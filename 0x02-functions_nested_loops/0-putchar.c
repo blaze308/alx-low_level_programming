c1cb8766b3f68d99932e2a66a156f04fb48b30b3
blaze308 <jaimepinkrah@gmail.com>
2023-02-21 05:58:10 -0800

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "jay.h"
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - print _putchar
+* return 0
+*/
+
 int main(void)
 {
-	char *sh = "jay";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
 }
