c92540d9e0f5243ce3343a39770378f0afbe5a26
blaze308 <jaimepinkrah@gmail.com>
2023-02-22 00:28:05 -0800

    thihs is fun

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _print_sign - function to print sign
+* print_sign - function to print sign
 * @n: parameter to check
 * Return: 1, 0 and -1 based on condition
 */
@@ -10,22 +10,16 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-_putchar(',');
-_putchar(' ');
 return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-_putchar(',');
-_putchar(' ');
 return (0);
 }
 else
 {
 _putchar('-');
-_putchar(',');
-_putchar(' ');
 return (-1);
 }
 }
